79579b4ac5f316dd1d7a26b49fc58ef75ed9fa08
KiteCaptain <97241357+KiteCaptain@users.noreply.github.com>
2022-09-18 08:26:57 +0300

    used axios library for data fetching

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import axios from 'axios';
 
 
 const useSemiPersistentState = (key, initialState) => {
@@ -81,12 +82,11 @@ const App = () => {
 
     dispatchStories({type: 'STORIES_FETCH_INIT'});
 
-    fetch(url)
-    .then((response) => response.json())
+    axios.get(url)
     .then((result) => {
       dispatchStories({
         type: 'STORIES_FETCH_SUCCESS',
-        payload: result.hits,
+        payload: result.data.hits,
     });
   })
     .catch(() => 
